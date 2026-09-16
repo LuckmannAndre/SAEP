7f9a7374f2157c87946c50efa3f0c84a3d289643
ANDRÉ LUCKMANN MARCOS <andre_l_marcos@estudante.sesisenai.org.br>
2026-09-16 12:28:35 -0300

    Adicionando MAIS rotas

--- backend/app.js
+++ backend/app.js
@@ -20,40 +20,80 @@ server.get('/produtos' , (req, res) =>{
 });
 
 
+
+
 server.get('/produtos/ordenados', (req, res) => {
-    const sql = 'SELECT * FROM PRODUTOS ORDER BY nome ASC';
+
+    const sql = 'SELECT * FROM PRODUTO ORDER BY nome ASC';
+
     connection.query(sql, (erro, resultados) => {
         if(erro){
-            return res.status(500).json({erro: erro.message});
+            return res.status(500).json({ erro: erro.message });
         }
         return res.json(resultados);
-    })
-})
+    });
+});
+
 
 server.get('/produtos/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = 'SELECT * FROM PRODUTOS WHERE id = ?';
-    connection.query(sql, [id], (erro, resultados) => {
+
+    const {id} = req.params;
+
+    const sql = 'SELECT * FROM PRODUTO WHERE id_produto = ?';
+
+    connection.query(sql, {id}, (erro, resultados) => {
         if(erro){
-            return res.status(500).json({erro: erro.message});
+            return res.status(500).json({ erro: erro.message})
         }
+
         return res.json(resultados);
-    })
-})
+    });
+});
+
+
 
 server.get('/produtos/busca/:nome', (req, res) => {
 
-    const termobusca = '%' + req.params.nome + '%';
+    const sql = 'SELECT * FROM PRODUTO WHERE nome LIKE ?';
 
-    const sql = 'SELECT * FROM PRODUTOS WHERE nome LIKE ?';
+    const termoBusca = '%' + req.params.nome + '%';
 
-    connection.query(sql, {termobusca}, (erro, resultados) => {
-        if(erro){
-            return res.status(500).json({erro: erro.message});
+    connection.query(sql, {termoBusca}, (erro, resultados) => {
+        if(erro) {
+            return res.status(500).json({ erro: erro.message});
         }
-        return res.json(resultados);
+        res.json(resultados);
+    });
+});
+
+
+server.post('/produtos' , (req, res) => {
+    const {nome , cor , textura , peso , unidade_medida , aplicacao , 
+    data_validade , estoque_minimo , estoque_atual , preco_unitario , id_categoria} = req.body
+
+    if (nome == null  || cor == null  || textura == null  ||  peso == null  ||  
+        unidade_medida == null  ||  aplicacao == null ||  data_validade == null  ||  
+        estoque_atual == null || estoque_minimo == null || preco_unitario == null || id_categoria == null
+    
+    ){
+        return res.status(400).json({erro: 'Todos os campos devem ser preenchidos'})
+    }
+
+    const sql = 'INSERT INTO PRODUTO (nome , cor , textura , peso , unidade_medida , aplicacao , data_validade , estoque_minimo , estoque_atual , preco_unitario , id_categoria) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+
+    connection.query(sql , [nome , cor , textura , peso , unidade_medida , aplicacao , 
+    data_validade , estoque_minimo , estoque_atual , preco_unitario , id_categoria] , (erro, resultado) => {
+
+        if(erro){
+            return res.status(500).json({erro: erro.message})
+        }
+        res.json({
+            mensagem: 'Produto Cadastrado com sucesso',
+            id: resultado.insertId
+        })
     })
 })
+
 
 const PORT = 3025;
 
